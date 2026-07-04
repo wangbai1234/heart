3ad14606b62bb7c44e764216167f876979590174
wangbai1234 <1513913499@qq.com>
2026-07-05 03:32:38 +0800

    fix: 引导页背景图继续放大 52vh→56vh

--- web/src/pages/OnboardingPage.tsx
+++ web/src/pages/OnboardingPage.tsx
@@ -99,7 +99,7 @@ export function OnboardingPage() {
                 className="relative flex h-full shrink-0 flex-col"
                 style={{ width: `${100 / steps.length}%` }}
               >
-                <div className="relative h-[52vh] min-h-[380px] w-full overflow-hidden">
+                <div className="relative h-[56vh] min-h-[400px] w-full overflow-hidden">
                   <img
                     src={step.image}
                     alt={step.title}
